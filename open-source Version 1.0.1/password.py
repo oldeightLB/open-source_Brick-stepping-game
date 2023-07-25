d424518fa4ec3d5299891fea49a5cd8286d74fa4
oldeightLB <109260128+oldeightLB@users.noreply.github.com>
2023-07-25 17:39:34 +0800

    Change Initial developer debugging password

--- open-source Version 1.0.1/password.py
+++ open-source Version 1.0.1/password.py
@@ -1,1 +1,3 @@
-password = 'ljmn94513'
+# Customize your developer password here 
+# Warning: The initial password is too simple, and not changing it may cause players to cheat!
+password = '123456'
